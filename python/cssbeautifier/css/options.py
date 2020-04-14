d864fb3ae85b76b007e1f39aae0ee08cdc8a95f1
Ben Howes <ben@ben-howes.co.uk>
2020-04-14 15:26:17 +0100

    run pre-commit on all files

--- python/cssbeautifier/css/options.py
+++ python/cssbeautifier/css/options.py
@@ -25,16 +25,23 @@
 
 from jsbeautifier.core.options import Options as BaseOptions
 
+
 class BeautifierOptions(BaseOptions):
     def __init__(self, options=None):
-        BaseOptions.__init__(self, options, 'css')
+        BaseOptions.__init__(self, options, "css")
 
-        self.selector_separator_newline = self._get_boolean('selector_separator_newline', True)
-        self.newline_between_rules = self._get_boolean('newline_between_rules', True)
+        self.selector_separator_newline = self._get_boolean(
+            "selector_separator_newline", True
+        )
+        self.newline_between_rules = self._get_boolean("newline_between_rules", True)
 
         # deprecated
-        space_around_selector_separator = self._get_boolean('space_around_selector_separator')
+        space_around_selector_separator = self._get_boolean(
+            "space_around_selector_separator"
+        )
 
         # Continue to accept deprecated option
-        self.space_around_combinator = self._get_boolean('space_around_combinator') or \
-            space_around_selector_separator
+        self.space_around_combinator = (
+            self._get_boolean("space_around_combinator")
+            or space_around_selector_separator
+        )
